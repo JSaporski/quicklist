8fdc4671c077ecf318cf55c7557699196a25aa8c
João <joaoguilherme@gmail.com>
2025-02-23 11:21:41 -0300

    Quicklist v1.0!

---
+++ main.js
@@ -0,0 +1,75 @@
+// Variáveis
+const form = document.querySelector('form')
+const input = form.querySelector('input')
+const list = document.querySelector('.list')
+const warning = document.querySelector('.warning')
+let items = []
+let itemID = 0
+
+// Adicionar item na lista
+function createItem(itemName, itemID) {
+  if (!itemName || itemName === ' ') {
+    return alert('Por favor, não deixe o nome vazio!')
+  }
+
+  const structure = `
+    <div class="wrap">
+      <input type="checkbox" name="check-item" id="item-${itemID}" />
+      <label for="item-${itemID}">${itemName}</label>
+    </div>
+
+    <button class="delete">
+      <img src="./assets/icon-delete.svg" alt="" />
+    </button>
+  `
+
+  const newItem = document.createElement('li')
+  newItem.innerHTML = structure
+
+  return newItem
+}
+
+function addItem(newItem) {
+  items.push(newItem)
+  list.prepend(newItem)
+
+  itemID += 1
+}
+
+function deleteItem(newItem) {
+  const btnDelete = newItem.querySelector('.delete')
+
+  btnDelete.addEventListener('click', () => {
+    const index = items.indexOf(newItem)
+    const closeWarning = warning.querySelector('.close')
+
+    items.splice(index, 1)
+    newItem.remove()
+
+    warning.classList.toggle('active')
+
+    closeWarning.addEventListener('click', () => {
+      warning.classList.remove('active')
+    })
+
+    setTimeout(() => {
+      warning.classList.remove('active')
+    }, 3500)
+  })
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const itemName = input.value.replace(/\s+/g, ' ')
+  const newItem = createItem(itemName, itemID)
+
+  input.value = ''
+
+  if (!newItem) {
+    return
+  }
+
+  addItem(newItem)
+  deleteItem(newItem)
+})
